1cb95040106da6c943f6c4d6bdd93b936525609d
lianet <jack.tolmak@yandex.ru>
2024-06-09 12:39:03 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,5 @@
 "use strict";
 
-
-import * as devFunctions from './modules/functions.js';
-
 //  init Fancybox
 if (typeof Fancybox !== "undefined" && Fancybox !== null) {
     Fancybox.bind("[data-fancybox]", {
@@ -401,6 +398,159 @@ $(function () {
     //     })
     // };
 
+
+
+    // input mask telephone russia
+    var phoneInputs = document.querySelectorAll('input[type="tel"]');
+
+    var getInputNumbersValue = function (input) {
+        return input.value.replace(/\D/g, '');
+    }
+    var onPhonePaste = function (e) {
+        var input = e.target,
+            inputNumbersValue = getInputNumbersValue(input);
+        var pasted = e.clipboardData || window.clipboardData;
+        if (pasted) {
+            var pastedText = pasted.getData('Text');
+            if (/\D/g.test(pastedText)) {
+
+                input.value = inputNumbersValue;
+                return;
+            }
+        }
+    }
+    var onPhoneInput = function (e) {
+        var input = e.target,
+            inputNumbersValue = getInputNumbersValue(input),
+            selectionStart = input.selectionStart,
+            formattedInputValue = "";
+
+        if (!inputNumbersValue) {
+            return input.value = "";
+        }
+
+        if (input.value.length != selectionStart) {
+
+            if (e.data && /\D/g.test(e.data)) {
+
+                input.value = inputNumbersValue;
+            }
+            return;
+        }
+
+        if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+            if (inputNumbersValue[0] == "9") inputNumbersValue = "7" + inputNumbersValue;
+            var firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7";
+            formattedInputValue = input.value = firstSymbols + " ";
+            if (inputNumbersValue.length > 1) {
+                formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
+            }
+            if (inputNumbersValue.length >= 5) {
+                formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
+            }
+            if (inputNumbersValue.length >= 8) {
+                formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+            }
+            if (inputNumbersValue.length >= 10) {
+                formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+            }
+        } else {
+            formattedInputValue = '+' + inputNumbersValue.substring(0, 16);
+        }
+        input.value = formattedInputValue;
+    }
+    var onPhoneKeyDown = function (e) {
+
+        var inputValue = e.target.value.replace(/\D/g, '');
+        if (e.keyCode == 8 && inputValue.length == 1) {
+            e.target.value = "";
+        }
+    }
+    for (var phoneInput of phoneInputs) {
+        phoneInput.addEventListener('keydown', onPhoneKeyDown);
+        phoneInput.addEventListener('input', onPhoneInput, false);
+        phoneInput.addEventListener('paste', onPhonePaste, false);
+    }
+
+
+    // validation fields
+
+    // $.validator.addMethod("onlyletters", function (value, element) {
+    //     return !/[\d!@#$%^&*(),.?":{}|<>]/.test(value);
+    // }, "Поле не должно содержать цифры");
+
+    // $.validator.addMethod("intlTelInputValid", function (value, element) {
+    //     var iti = window.intlTelInputGlobals.getInstance(element);
+    //     return this.optional(element) || iti.isValidNumber();
+    // }, "Пожалуйста, введите корректный номер телефона");
+
+    $.validator.addMethod("phoneRU", function (value, element) {
+        return this.optional(element) || /^(8|\+7) \(\d{3}\) \d{3}-\d{2}-\d{2}$/.test(value);
+    }, "");
+
+
+    if ($('[data-validation-form]').length > 0) {
+
+
+        $('[data-validation-form]').each(function () {
+
+            $(this).validate({
+                rules: {
+
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    phone: {
+                        required: true,
+                        phoneRU: true
+                    },
+
+                },
+                messages: {
+                    email: {
+                        required: "Это поле необходимо заполнить.",
+                        email: "Пожалуйста, введите корректный адрес электронной почты."
+                    },
+                    phone: {
+                        required: "Это поле необходимо заполнить."
+                    }
+                },
+                showErrors: function (errorMap, errorList) {
+
+                    this.defaultShowErrors();
+
+                    $.each(errorList, function (index, error) {
+                        if (error.method === "phoneRU") {
+                            $(error.element).next('label.error').addClass('hidden');
+                        }
+                    });
+
+                    $.each(this.currentElements, function (index, element) {
+                        if ($(element).hasClass('error') && $(element).attr('name') === 'phone' && $(element).val() === '') {
+                            $(element).next('label.error').removeClass('hidden');
+                        }
+                    });
+                }
+
+            });
+        });
+
+
+        function checkFormValidity(form) {
+
+            if (form.valid()) {
+                form.find("button[type='submit']").removeAttr('disabled');
+            } else {
+                form.find("button[type='submit']").attr('disabled', 'disabled');
+            }
+        }
+
+        $('[data-validation-form] input, [data-validation-form] select, [data-validation-form] textarea').on('keyup change', function () {
+
+            checkFormValidity($(this).closest('form'));
+        });
+    }
 
 
 
