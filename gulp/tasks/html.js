c23442e93a85b6f89bef64b1248e201daa00d07f
lianet <jack.tolmak@yandex.ru>
2024-06-12 19:32:05 +0300

    commit

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -6,7 +6,19 @@ import htmlBeautify from "gulp-html-beautify";
 export const html = () => {
     return app.gulp
         .src(app.path.src.html)
-        .pipe(app.plugins.if(app.isBuild, formatHTML()))
+        .pipe(
+            app.plugins.if(
+                app.isBuild,
+                htmlBeautify({
+                    indent_size: 4,
+                    indent_with_tabs: true,
+                    preserve_newlines: false,
+                    max_preserve_newlines: 0,
+                    wrap_line_length: 0,
+                    end_with_newline: true
+                })
+            )
+        )
         .pipe(
             app.plugins.plumber(
                 app.plugins.notify.onError({
